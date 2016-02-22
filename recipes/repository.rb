a1b02ba6eec1455d781053b64ed2bf01c85e2baa
Nicolas Degory <ndegory@axway.com>
2016-02-22 17:05:24 +0000

    add amazon linux platform for centos repos

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -46,7 +46,7 @@ when 'ubuntu'
       File.exist?("/etc/apt/sources.list.d/glusterfs-#{node['gluster']['version']}.list")
     end
   end
-when 'redhat', 'centos'
+when 'redhat', 'centos', 'amazon'
   # CentOS 6 and 7 have Gluster in the Storage SIG instead of a gluster hosted repo
   repo_url = if node['platform_version'].to_i > 5
                "http://mirror.centos.org/centos/$releasever/storage/$basearch/gluster-#{node['gluster']['version']}/"
